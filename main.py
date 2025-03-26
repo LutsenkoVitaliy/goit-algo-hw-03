86825cc3bb116129235c8b4507718de0637a7062
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-03-26 16:35:23 +0200

    firsandtwocomplete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Перше завдання
+# Перше завдання ----------------------------------------
 import datetime
 
 now = datetime.datetime.now()
@@ -7,21 +7,25 @@ def get_days_from_today(date):
   diference = now.date().toordinal() - date.toordinal()
   return diference
   
-#print(get_days_from_today(datetime.date(2028, 9, 24)))
-#print(get_days_from_today(datetime.date(1996, 9, 24)))
+print(get_days_from_today(datetime.date(2028, 9, 24)))
+print(get_days_from_today(datetime.date(1996, 9, 24)))
 
-# Друге завдання
+# Друге завдання ------------------------------------------
 import random
 
 def get_numbers_ticket(min, max, quantity):
-  #lottery_numbers = random.randint(min, max)
+  lottery_numbers = []
 
-  for lottery_numbers in range(quantity):
-    pass
-    # print(random.randint(min, max))
+  for _ in range(quantity):
+    lottery_numbers.append(random.randint(min, max))
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
-print(f"Ваші лотерейні числа: {lottery_numbers}")
+  return lottery_numbers
+
+print(f"Ваші лотерейні числа: {get_numbers_ticket(1, 49, 6)}")
 
 
-print("fdsfsdf")
+# Третє завдання ------------------------------------------
+import re
+
+def normalize_phone(phone_number):
+  pass
