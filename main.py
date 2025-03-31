0078adb250b6fca3dde6466a15724340f8ad21d3
LutsenkoVitaliy <lutsenko996@gmail.com>
2025-03-31 19:19:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,23 @@ print(f"Ваші лотерейні числа: {get_numbers_ticket(1, 49, 6)}")
 # Третє завдання ------------------------------------------
 import re
 
-def normalize_phone(phone_number):
-  pass
+def normalize_phone(phone_number: str) -> str:
+  phone_number = phone_number.strip().replace(" ", "") # видаляэмо всі пробіли
+
+
+
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
